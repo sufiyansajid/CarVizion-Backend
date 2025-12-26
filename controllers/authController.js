5ee11216da1321c0bfefae04df31fe98ed7665c7
sufiyansajid <sufiyansajid0000@gmail.com>
2025-12-26 16:00:17 +0500

    fixed auth

--- controllers/authController.js
+++ controllers/authController.js
@@ -88,7 +88,8 @@ const authController = {
                 token,
                 user: {
                     id: user.id,
-                    name: user.name,
+                    firstName: user.first_name,
+                    lastName: user.last_name,
                     email: user.email
                 }
             });
